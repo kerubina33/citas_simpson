29ee16142a1fa02ac20acf01eadb0e3f9e88f1d0
kerubina33 <sonia.per.mar@gmail.com>
2016-11-28 14:16:44 +0100

    Citas simpson

---
+++ www/js/galeriaInicial.js
@@ -0,0 +1,26 @@
+var galeria = [
+   { persona:"Hommer Simpson",
+     frase:"“Para Empezar Presionar Cualquier Tecla” ¿Dónde está la tecla “Cualquier”?",
+     foto:"https://images.tenplay.com.au/~/media/TV%20Shows/The%20Simpsons/General/TheSimpsons_About_HomerSimpson.jpg"
+   },
+   { persona:"Lisa Simpson",
+     frase:"Me resisto a creer que todo el mundo se resiste a creer en la verdad.",
+     foto:"http://terriblerecords.com/wp-content/uploads/2015/04/11101986_357500327786879_1024023442_n.jpg"
+   },
+   { persona:"Bart Simpson",
+     frase: "Gracias a Dios recuperamos la razón y veneramos a un carpintero de hace dos mil años.",
+     foto:"http://67.media.tumblr.com/d7898f07373267273960e93b114db097/tumblr_nlf5piF0iE1uoecw1o1_1280.jpg"
+   },
+   { persona:"Marge Simpson",
+     frase:"El mudo va mucho mejor ahora que los cientificos han inventado la magia.",
+     foto:"http://4.bp.blogspot.com/_-TVOs_wFG4w/S-BkD_km-3I/AAAAAAAAADw/DjaIJ00T7Vs/s1600/marge-simpson.png"
+   },
+   { persona:"Krusty el payaso",
+     frase:"Prefiero ser un crédulo feliz, que un noble tonto.",
+     foto:"http://blogs.lainformacion.com/strambotic/files/2012/10/Krusty2.gif"
+   },
+   { persona:"Montgomery Burns",
+     frase:"¿De qué sirve tener dinero si no puedes inspirarle terror a tus semejantes?",
+     foto:"http://vignette4.wikia.nocookie.net/inciclopedia/images/6/68/Burncito.gif/revision/latest?cb=20090513222042"
+   }
+];
